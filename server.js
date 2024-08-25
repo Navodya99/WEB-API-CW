d04e54169bee8ff47e8c7f8fca7065264c6b433b
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-08-25 21:50:39 +0530

    Mongo db support added

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 const PORT = 8090;
@@ -12,6 +16,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "My Train API" });
 });
+
+// MongoDB URL from environment variables
+const mongoDBUrl = process.env.MONGODB_URL;
+
+console.log("MongoDB URL:", mongoDBUrl);
 
 // Start the server
 app.listen(PORT, () => {
