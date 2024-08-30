4f6014104d09f20176d4b445ce3dad4aba9022cf
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-08-30 23:31:40 +0530

    Coordinates added

--- Models/Train.js
+++ Models/Train.js
@@ -1,40 +1,46 @@
 const mongoose = require("mongoose");
 
-
 const trainSchema = new mongoose.Schema({
- trainName: {
-   type: String,
-   required: true,
- },
- line: {
-   type: String,
-   enum: ["Main Line", "Puttalam Line", "Coastal Line", "Kalaniwali Line"],
-   required: true,
- },
- currentStation: {
-   type: mongoose.Schema.Types.ObjectId,
-   ref: "Station",
-   required: true,
- },
- lastPassedTime: {
-   type: Date,
-   required: true,
- },
- passedStations: [
-   {
-     station: {
-       type: mongoose.Schema.Types.ObjectId,
-       ref: "Station",
-     },
-     time: Date,
-   },
- ],
- status: {
-   type: String,
-   enum: ["Running", "Stopped"],
-   default: "Running",
- },
+  trainName: {
+    type: String,
+    required: true,
+  },
+  line: {
+    type: String,
+    enum: ["Main Line", "Puttalam Line", "Coastal Line", "Kalaniwali Line"],
+    required: true,
+  },
+  currentStation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Station",
+    required: true,
+  },
+  currentCoordinates: {
+    type: [Number],
+    index: "2dsphere",
+  },
+  lastPassedTime: {
+    type: Date,
+    required: true,
+  },
+  passedStations: [
+    {
+      station: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Station",
+      },
+      time: Date,
+      coordinates: {
+        type: [Number],
+        index: "2dsphere",
+      },
+    },
+  ],
+  status: {
+    type: String,
+    enum: ["Running", "Stopped"],
+    default: "Running",
+  },
 });
-
 
 module.exports = mongoose.model("Train", trainSchema);
