923b3acefbc43e3ef09c2cc1d136ca5725ade3b2
Navodya Weerasooriya <58646038+Navodya99@users.noreply.github.com>
2024-08-30 23:34:47 +0530

    minor changes

--- Models/Station.js
+++ Models/Station.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
-
 const stationSchema = new mongoose.Schema({
- name: {
-   type: String,
-   required: true,
- },
- 
- line: {
-   type: String,
-   enum: ["Main Line", "Puttalam Line", "Coastal Line", "Kalaniwali Line"],
-   required: true,
- },
+  name: {
+    type: String,
+    required: true,
+  },
+  line: {
+    type: String,
+    enum: ["Main Line", "Puttalam Line", "Coastal Line", "Kalaniwali Line"],
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
 });
 
-
 module.exports = mongoose.model("Station", stationSchema);
-
-
